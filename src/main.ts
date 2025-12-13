8f9e5d2200686db66479bc1991ee8ce8511e7f0a
Gaziav <bebraziana@gmail.com>
2025-12-13 16:57:55 +0300

    Guest

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,36 @@
 import { faker } from '@faker-js/faker';
-let a = faker.animal.cat();
-let b = faker.vehicle.bicycle()
-console.log(a);
-console.log(b);
+type UserRole = 'admin' | 'guest' | 'manager';
+
+type User = {
+    id: string;
+    email: string;
+    password: string;
+    role: UserRole;
+}
+function generateRandomUser(n: number): User[] {
+    const users = [];
+    const roles = ['admin', 'guest', 'manager'];
+    for(let i = 0; i < n; i++) {
+        const random = Math.floor(Math.random() * roles.length);
+        const randomRole = roles[random];
+        const user: User = {
+            id: faker.string.uuid(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            role: randomRole
+        }
+        users.push(user);
+    }
+    return users;
+}
+function filterByRole(users: User[], role: UserRole[]): User[] {
+    const user = [];
+    for (let i = 0; i < user.length; i++) {
+        const user = user[i];
+        if (user.role === role) {
+        }
+        user.push(user[i]);
+    }
+    return user;
+}
+console.log(generateRandomUser(5));
