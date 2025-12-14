bcdeba337c5a736478266fa71ff3d7e1923ad47e
Gaziav <bebraziana@gmail.com>
2025-12-14 19:44:01 +0300

    ИТЕРАЦИЯ 2

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,38 @@
 type Options = {
-    length: number;
+    length: number,
+    lowercase: boolean,
+    uppercase: boolean,
+    numbers: boolean,
+    symbols: boolean,
 }
-function generatePassword(options: Options) {
-    const { length } = options;
+function generatePassword2(options: Options) {
+    const { length, lowercase, uppercase, numbers, symbols } = options;
     if (length <= 0) {
         return null;
     }
+    let chars = '';
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numbers = '0123456789';
-    const all = alphabet + uppercase + numbers;
-    let password= '';
+    const uppercase1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const number = '0123456789';
+    const symbol = '!@#$%^&*()_+{}|[];:,.<>?';
+    if (lowercase) chars += alphabet;
+    if (uppercase) chars += uppercase1;
+    if (numbers) chars += number;
+    if (symbols) chars += symbol;
+    if (chars.length === 0) {
+        return '';
+    }
+    let password = '';
     for (let i = 0; i < length; i++) {
-        const random = Math.floor(Math.random() * all.length);
-        password += all[random]
+        const random = Math.floor(Math.random() * chars.length);
+        password += chars[random];
     }
     return password;
 }
-console.log(generatePassword({ length: 10 }));
+console.log(generatePassword2({
+    length: 10,
+    lowercase: false,
+    uppercase: true,
+    numbers: false,
+    symbols: true
+}));
