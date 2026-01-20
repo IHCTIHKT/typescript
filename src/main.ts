9f3e23f3bfe2bdd4ccd982652158cfc7044308b4
Gaziav <bebraziana@gmail.com>
2026-01-20 18:16:29 +0300

    Самый дорогой заказ.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { writeFileSync, readFileSync} from 'fs';
 
-const poem = readFileSync('./text.txt', 'utf8');
-let newPoem = '';
-for (let letter of poem) {
-    if (letter === 'а') newPoem += 'о';
-    else if (letter === 'А') newPoem += 'О';
-    else if (letter === 'о') newPoem += 'а';
-    else if (letter === 'О') newPoem += 'А';
-    else newPoem += letter;
+const fileName = readFileSync('order.csv', 'utf8');
+const line = fileName.split('\n');
+const days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+let maxOrder = 0;
+let maxDay = '';
+let position = 0;
+
+for (let i = 0; i < line.length; i++) {
+    const number = line[i].split(',').map(Number);
+    const day = days[i % 7];
+
+    for (let k = 0; k < number.length; k++) {
+        const thisNumber = number[k];
+        if (thisNumber > maxOrder) {
+            maxOrder = thisNumber;
+            maxDay = day;
+            position = k + 1;
+        }
+    }
 }
-writeFileSync('./text2.txt', newPoem);
-console.log(newPoem);
+console.log(` Наибольший заказ - ${maxOrder}`);
+console.log(` Он сделан в ${maxDay}, по порядку #${position}`);
