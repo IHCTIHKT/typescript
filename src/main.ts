08054b015236b59c35f9c1a9bb67af5e01ac3ead
Gaziav <bebraziana@gmail.com>
2026-01-20 19:49:38 +0300

    числа

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,20 @@
 /*
-Вам дан список сотрудников компании.
-У каждого сотрудника, кроме генерального директора, есть начальник.
 
-Описание полей:
-id - id самого сотрудника
-name - имя самого сотрудника
-title - должность текущего сотрудника
-chiefId - id сотрудника, который является начальником текущего сотрудника
+Напишите функцию intersection, которая получает на вход 2 массива чисел.
+А возвращает те элементы, которые есть в обоих массивах (то есть общие элементы).
 
-
-Ваша задача - вывести цепочку сотрудников согласно их подчинению.
-Например, Алексей Иванов является генеральным директором, потому что у него нет начальник (chiefId=null),
-далее ищем его подчиненного, то есть такого сотрудника, у которого в chiefId указан гендир (chiefId=42).
-И так далее, пока не выведем всех сотрудников компании.
-
-Гарантируется, что цепочка подчинения линейная, то есть у каждого человека может быть
-только один другой сотрудник в подчинении, и у каждого сотрудника может быть только один начальник.
-
-Вывод должен быть таким:
-
-Алексей Иванов (Chief Executive Officer)
-Ирина Петрова (co-Chief Executive Officer)
-Дмитрий Соколов (Chief Operating Officer)
-Екатерина Смирнова (Chief Product and Technology Officer)
-Сергей Кузнецов (Chief Technical Officer)
-Ольга Васильева (Tech Lead)
-Николай Михайлов (Team Lead)
-Марина Федорова (Backend Developer)
  */
+function intersection(arr1: number[], arr2: number[]) {
+    const result: number[] = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let number = arr1[i];
 
-type User = {
-    id: number;
-    name: string;
-    chiefId: number | null;
-    title: string;
-};
-
-const employees: User[] = [
-    { id: 59, name: 'Екатерина Смирнова', chiefId: 12, title: 'Chief Product and Technology Officer' },
-    { id: 4, name: 'Николай Михайлов', chiefId: 34, title: 'Team Lead' },
-    { id: 12, name: 'Дмитрий Соколов', chiefId: 87, title: 'Chief Operating Officer' },
-    { id: 96, name: 'Марина Федорова', chiefId: 4, title: 'Backend Developer' },
-    { id: 71, name: 'Сергей Кузнецов', chiefId: 59, title: 'Chief Technical Officer' },
-    { id: 34, name: 'Ольга Васильева', chiefId: 71, title: 'Tech Lead' },
-    { id: 42, name: 'Алексей Иванов', chiefId: null, title: 'Chief Executive Officer' },
-    { id: 87, name: 'Ирина Петрова', chiefId: 42, title: 'co-Chief Executive Officer' },
-];
-let bossChief: number | null = null;
-for (let i = 0; i < employees.length; i++) {
-    if (employees[i].chiefId === null) {
-        bossChief = employees[i].id;
-    }
-}
-while (bossChief !== null) {
-    let thisEmployees: User | null = null;
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].id === bossChief) {
-            thisEmployees = employees[i];
+        for(let j = 0; j < arr2.length; j++) {
+            if (number === arr2[j]) {
+                result.push(number);
+            }
         }
     }
-    let nextChiefId: number;
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].chiefId === nextChiefId) {
-            nextChiefId = employees[i].id;
-        }
-    }
-    bossChief = nextChiefId;
+    return result;
 }
+console.log(intersection([1, 2, 3], [2, 3, 4, 5]));
