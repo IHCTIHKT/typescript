9ae6254e0c95da3afd6cb7983d61935f7780f80f
Gaziav <bebraziana@gmail.com>
2026-01-20 19:38:15 +0300

    сотрудники

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,69 @@
-import { readFileSync, writeFileSync } from 'fs';
+/*
+Вам дан список сотрудников компании.
+У каждого сотрудника, кроме генерального директора, есть начальник.
 
-const poem = readFileSync('./text.txt', 'utf8');
-let newPoem = '';
-for (let letter of poem) {
-    if (letter === 'а') newPoem += 'о';
-    else if (letter === 'А') newPoem += 'О';
-    else if (letter === 'о') newPoem += 'а';
-    else if (letter === 'О') newPoem += 'А';
-    else newPoem += letter;
+Описание полей:
+id - id самого сотрудника
+name - имя самого сотрудника
+title - должность текущего сотрудника
+chiefId - id сотрудника, который является начальником текущего сотрудника
+
+
+Ваша задача - вывести цепочку сотрудников согласно их подчинению.
+Например, Алексей Иванов является генеральным директором, потому что у него нет начальник (chiefId=null),
+далее ищем его подчиненного, то есть такого сотрудника, у которого в chiefId указан гендир (chiefId=42).
+И так далее, пока не выведем всех сотрудников компании.
+
+Гарантируется, что цепочка подчинения линейная, то есть у каждого человека может быть
+только один другой сотрудник в подчинении, и у каждого сотрудника может быть только один начальник.
+
+Вывод должен быть таким:
+
+Алексей Иванов (Chief Executive Officer)
+Ирина Петрова (co-Chief Executive Officer)
+Дмитрий Соколов (Chief Operating Officer)
+Екатерина Смирнова (Chief Product and Technology Officer)
+Сергей Кузнецов (Chief Technical Officer)
+Ольга Васильева (Tech Lead)
+Николай Михайлов (Team Lead)
+Марина Федорова (Backend Developer)
+ */
+
+type User = {
+    id: number;
+    name: string;
+    chiefId: number | null;
+    title: string;
+};
+
+const employees: User[] = [
+    { id: 59, name: 'Екатерина Смирнова', chiefId: 12, title: 'Chief Product and Technology Officer' },
+    { id: 4, name: 'Николай Михайлов', chiefId: 34, title: 'Team Lead' },
+    { id: 12, name: 'Дмитрий Соколов', chiefId: 87, title: 'Chief Operating Officer' },
+    { id: 96, name: 'Марина Федорова', chiefId: 4, title: 'Backend Developer' },
+    { id: 71, name: 'Сергей Кузнецов', chiefId: 59, title: 'Chief Technical Officer' },
+    { id: 34, name: 'Ольга Васильева', chiefId: 71, title: 'Tech Lead' },
+    { id: 42, name: 'Алексей Иванов', chiefId: null, title: 'Chief Executive Officer' },
+    { id: 87, name: 'Ирина Петрова', chiefId: 42, title: 'co-Chief Executive Officer' },
+];
+let bossChief: number | null = null;
+for (let i = 0; i < employees.length; i++) {
+    if (employees[i].chiefId === null) {
+        bossChief = employees[i].id;
+    }
 }
-writeFileSync('./text2.txt', newPoem);
-console.log(newPoem);
+while (bossChief !== null) {
+    let thisEmployees: User | null = null;
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].id === bossChief) {
+            thisEmployees = employees[i];
+        }
+    }
+    let nextChiefId: number;
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].chiefId === nextChiefId) {
+            nextChiefId = employees[i].id;
+        }
+    }
+    bossChief = nextChiefId;
+}
