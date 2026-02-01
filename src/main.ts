6022d2a47decee03b632b65fa454ef4be31aa51d
Gaziav <bebraziana@gmail.com>
2026-02-01 18:04:20 +0300

    Пропущенные числа.

--- src/main.ts
+++ src/main.ts
@@ -1,76 +1,21 @@
-/*
-Вам дан список сотрудников компании.
-У каждого сотрудника, кроме генерального директора, есть начальник.
+//Напишите функцию, которая получает на вход отсортированный массив чисел от 1 до N и возвращает пропущенные числа.
+    //Если массив, полученный на вход, не отсортирован, необходимо вернуть null.
 
-Описание полей:
-id - id самого сотрудника
-name - имя самого сотрудника
-title - должность текущего сотрудника
-chiefId - id сотрудника, который является начальником текущего сотрудника
-
-
-Ваша задача - вывести цепочку сотрудников согласно их подчинению.
-Например, Алексей Иванов является генеральным директором, потому что у него нет начальник (chiefId=null),
-далее ищем его подчиненного, то есть такого сотрудника, у которого в chiefId указан гендир (chiefId=42).
-И так далее, пока не выведем всех сотрудников компании.
-
-Гарантируется, что цепочка подчинения линейная, то есть у каждого человека может быть
-только один другой сотрудник в подчинении, и у каждого сотрудника может быть только один начальник.
-
-Вывод должен быть таким:
-
-Алексей Иванов (Chief Executive Officer)
-Ирина Петрова (co-Chief Executive Officer)
-Дмитрий Соколов (Chief Operating Officer)
-Екатерина Смирнова (Chief Product and Technology Officer)
-Сергей Кузнецов (Chief Technical Officer)
-Ольга Васильева (Tech Lead)
-Николай Михайлов (Team Lead)
-Марина Федорова (Backend Developer)
- */
-
-type User = {
-    id: number;
-    name: string;
-    chiefId: number | null;
-    title: string;
-};
-
-const employees: User[] = [
-    { id: 59, name: 'Екатерина Смирнова', chiefId: 12, title: 'Chief Product and Technology Officer' },
-    { id: 4, name: 'Николай Михайлов', chiefId: 34, title: 'Team Lead' },
-    { id: 12, name: 'Дмитрий Соколов', chiefId: 87, title: 'Chief Operating Officer' },
-    { id: 96, name: 'Марина Федорова', chiefId: 4, title: 'Backend Developer' },
-    { id: 71, name: 'Сергей Кузнецов', chiefId: 59, title: 'Chief Technical Officer' },
-    { id: 34, name: 'Ольга Васильева', chiefId: 71, title: 'Tech Lead' },
-    { id: 42, name: 'Алексей Иванов', chiefId: null, title: 'Chief Executive Officer' },
-    { id: 87, name: 'Ирина Петрова', chiefId: 42, title: 'co-Chief Executive Officer' },
-];
-let bossChief: number | null = null;
-
-for (let i = 0; i < employees.length; i++) {
-    if (employees[i].chiefId === null) {
-        bossChief = employees[i].id;
-    }
-}
-
-while (bossChief !== null) {
-    let thisEmployee: User | null = null;
-
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].id === bossChief) {
-            thisEmployee = employees[i];
+function getMissingDigit(arr: number[]): number | null {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return null;
         }
     }
-    if (thisEmployee) {
-        console.log(`${thisEmployee.name} (${thisEmployee.title})`);
-    }
-    let nextChiefId: number | null = null;
-    for (let i = 0; i < employees.length; i++) {
-        if (employees[i].chiefId === bossChief) {
-            nextChiefId = employees[i].id;
+    const result: number[] = [];
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+    for (let i = 0; i < arr.length; i++) {
+        while (first < arr[i]) {
+            result.push(first);
+            first++;
         }
     }
-
-    bossChief = nextChiefId;
+    return result;
 }
+console.log(getMissingDigit([1, 2, 3, 4, 5, 8, 10, 11, 12, 15]));
