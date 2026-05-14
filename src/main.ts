8af9c410864ff187ddbc56f1389e9023cf52665b
Gaziav <bebraziana@gmail.com>
2026-05-14 19:39:25 +0300

    скобки (поправка)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-function includes(text, numbers) {
-    if (numbers.length > text.length) return false;
+function correct(str: string): boolean {
+    let result = str;
 
-    const countNumbers = {};
-    for (let number of numbers) {
+    while (result.includes('()') || result.includes('[]') || result.includes('{}')) {
+        result = result.replace('()', '');
+        result = result.replace('[]', '');
+        result = result.replace('{}', '');
+    }
+    return result.length === 0;
+}
+console.log(correct('()'));
+console.log(correct('({})')); // true
+console.log(correct('{[({()})]})')); // true
+console.log(correct('(){}([])[[[]]]{}()')); // true
+
+console.log(correct('(({})')); // false
+console.log(correct('({}))')); // false
+console.log(correct('([})')); // false
+console.log(correct(')(')); // false
+console.log(correct(')(}][{')); // false
+console.log(correct('({[]})'));
+console.log(correct('()()()()()()()()()()()()()()()()()()()()()()()()()()()(){}{}[]{}{[]}{{}}()'))
+console.log(correct('{[({()})]}'));
